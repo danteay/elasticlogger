fcd2a22c349da076bbecd9ff9cb06e0439d9436e
Eduardo Aguilar <eduardoay@outlook.com>
2020-05-14 20:03:47 +0000

    Sentry

--- docs/quickstart.rst
+++ docs/quickstart.rst
@@ -1,8 +1,11 @@
-Quick Satrt
+Quick Start
 ===========
 
-Elastic logger use the standard python logging package and the pythonjsonlogger package
-to get a standarized logger that can be compatible with elastic search.
+Basic config
+------------
+
+Elasticlogger use the standard python logging package and the python_json_logger package
+to get a standardized logger that can be compatible with elastic search and Sentry.
 
 The way to create a simple logger is by following the next example:
 
@@ -13,22 +16,40 @@ The way to create a simple logger is by following the next example:
 
     logger = Logger("test-logger", level=logging.DEBUG)
 
-This will create a logger instance with DEBUG level.
-
-* You can simple log like this:
+This will create a logger instance with DEBUG level and you can simply log like this:
 
 .. code-block:: python
 
     logger.debug("test logger message")
-    # {"message": "testlogger message", "levelname": "DEBUG", "name": "test-logger"}
+    # {"message": "test logger message", "levelname": "DEBUG", "name": "test-logger"}
 
-* To enable the elasticsearch integration yo need to call the next function
+ElasticSearch Integration
+-------------------------
+
+To enable the ElasticSearch integration you need to call the next function with params
+`endpoint` and `index`, where endpoint is the elastic cluster url and the index will be
+the default index where the logs will be stored.
 
 .. code-block:: python
 
-    # setup the elasticsearch endpoint of your cluster and the default index
+    # Setup the ElasticSearch endpoint of your cluster and the default index
     logger.enable_elastic(endpoint="https://elastic-cluster.com", index="test-index")
 
-    # you can simply call the function and the data will be take from the envvars
-    # ELASTIC_sEARCH_ENDPOINT and ELASTIC_sEARCH_INDEX
+    # You can simply call the function and the data will be take from the env vars
+    # ELASTIC_SEARCH_ENDPOINT and ELASTIC_SEARCH_INDEX
     logger.enable_elastic()
+
+Sentry Integration
+------------------
+To enable sentry integration you simply need to cal the next method
+
+.. code-block:: python
+
+    # Setup Sentry project URL and integrations
+    logger.enable_sentry(endpoint="https://sentry-url.com/project-id")
+
+    # Also you can just call the method and take the endpoint value from the env var
+    # SENTRY_URL like this
+    logger.enable_sentry()
+
+
--- elasticlogger/__init__.py
+++ elasticlogger/__init__.py
@@ -9,21 +9,29 @@ from elasticsearch import Elasticsearch
 from pythonjsonlogger import jsonlogger
 
 import certifi
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration, ignore_logger
 
 
 class Logger:
-    """Custom logger that interact with pythonjsonlogger and elastic search"""
+    """Custom logger that interact with python_json_logger and elastic search
+
+    :param name: (str) Logger name
+    :param level: Optional(str) Logger level (Def: DEBUG)
+    :param fmt: Optional(str) Log format fields (Def: %(asctime) %(levelname) %(name) %(message))
+    """
 
     def __init__(self, name, level=None, fmt=None):
         self.extra = {}
         self.elastic = None
+        self.sentry = None
         self.index = None
         self.logger = logging.getLogger(name)
 
         level = logging.DEBUG if not level else level
         handler = logging.StreamHandler()
 
-        fmt = u"%(message)%(levelname)%(name)" if not fmt else fmt
+        fmt = u"%(asctime) %(levelname) %(name) %(message)" if not fmt else fmt
 
         handler.setFormatter(jsonlogger.JsonFormatter(fmt))
 
@@ -31,14 +39,14 @@ class Logger:
         self.logger.setLevel(level)
 
     def enable_elastic(self, endpoint=None, index=None):
-        """Enable elasticsearch integration to stream logs. If you don't set endpoint and index
+        """Enable ElasticSearch integration to stream logs. If you don't set endpoint and index
         configurations this will try to get the configuration form the environment variables
         ELASTIC_SEARCH_ENDPOINT and ELASTIC_SEARCH_INDEX
 
-        :param endpoint: (str) Elasticsearch cluster endpoint
-        :param index: (str) Elasticsearch index where the logs will be streamed
+        :param endpoint: Optional(str) ElasticSearch cluster endpoint
+        :param index: Optional(str) ElasticSearch index where the logs will be streamed
         """
-        endpoint = endpoint if endpoint else os.getenv("ELASTIC_SEARCH_ENPOINT")
+        endpoint = endpoint if endpoint else os.getenv("ELASTIC_SEARCH_ENDPOINT")
 
         if not endpoint:
             return
@@ -48,8 +56,35 @@ class Logger:
         )
         self.index = index if index else os.getenv("ELASTIC_SEARCH_INDEX")
 
+    def enable_sentry(self, endpoint=None, integrations=None):
+        """Enable Sentry integration for realtime issue monitoring. If you don't set the endpoint
+        field it will be set from the env ver SENTRY_URL. On the integrations field you can set a
+        list of objects from the sentry_sdk.integrations package to be implemented.
+
+        :param endpoint: Optional(str) Sentry project endpoint
+        :param integrations: Optional(list) List of sentry_sdk.integrations objects
+        """
+
+        endpoint = endpoint if endpoint else os.getenv("SENTRY_URL")
+
+        if not endpoint:
+            return
+
+        sentry_logging = LoggingIntegration(
+            level=logging.DEBUG,  # Capture info and above as breadcrumbs
+            event_level=logging.ERROR,  # Send errors as events
+        )
+
+        base_integrations = [sentry_logging]
+
+        if integrations is not None:
+            integrations = integrations + base_integrations
+
+        ignore_logger("root")
+        self.sentry = sentry_sdk.init(endpoint, integrations=integrations)
+
     def fields(self, fields):
-        """Add muntiple extra fields to the json log string
+        """Add multiple extra fields to the json log string
 
         :param fields: (dict) Extra fields to add in the json log
         :return: (self)
@@ -65,7 +100,7 @@ class Logger:
         """Add a single field to the json log string
 
         :param name: (str) New key name for the field
-        :param value: (any) Value of the field (if it's an object needs to be json serializable)
+        :param value: (Any) Value of the field (if it's an object needs to be json serializable)
         :return: (self)
         """
 
@@ -73,7 +108,7 @@ class Logger:
         return self
 
     def debug(self, message):
-        """Print a debug log with all extra fields saved. The fileds will be cleaned after the
+        """Print a debug log with all extra fields saved. The fields will be cleaned after the
         logs are send
 
         :param message: (str) Log message
@@ -84,7 +119,7 @@ class Logger:
         self.extra = {}
 
     def info(self, message):
-        """Print an info log with all extra fields saved. The fileds will be cleaned after the
+        """Print an info log with all extra fields saved. The fields will be cleaned after the
         logs are send
 
         :param message: (str) Log message
@@ -106,10 +141,10 @@ class Logger:
 
     def error(self, message, error=None):
         """Print an error log with all extra fields saved, and with an specific error field. The
-        fileds will be cleaned after the logs are send
+        fields will be cleaned after the logs are send
 
         :param message: (str) Log message
-        :param error: (any) Exception or specific error message
+        :param error: Optional(Any) Exception or specific error message
         """
         trace = None
 
@@ -122,12 +157,12 @@ class Logger:
         self.extra = {}
 
     def _ensure_elastic(self, message, level, error=None, trace=None):
-        """Ensure elstic search syncronization, by checking configuration and colleting data
+        """Ensure elastic search synchronization, by checking configuration and collecting data
 
         :param message: (str) Principal message of the log
         :param level: (str) Log level
-        :param error: (str) Error to be logged
-        :param trace: (str) Last error trace
+        :param error: Optional(Any) Error to be logged
+        :param trace: Optional(str) Last error trace
         """
         if not self.elastic:
             return
